6b6d35615b2c4f8c7a4af1bab47b1b73af0b0e62
Tigran-griig <68404308+Tigran-griig@users.noreply.github.com>
2020-09-02 03:07:03 +0400

    Revert "Chat"

--- src/App.js
+++ src/App.js
@@ -1,58 +1,55 @@
 import React, { PureComponent }  from "react";
-// import { bindActionCreators } from "redux";
-// import { connect } from "react-redux";
-// import {
-//   Router,
-//   Route,
-//   Switch,
-//   Redirect
-// } from "react-router-dom";
-//
-// import browserHistory from "@/helpers/history";
-//
-// import Home from "@/pages/Home";
-// import Register from "@/pages/Register";
-// import Login from "@/pages/Login";
-// import { onLogout } from "@/stateManagement/userActionApi";
-// import PublicRoute from "@/components/router/PublicRouter";
-// import MainLayout from "@/components/layout/MainLayout";
-// import EmptyLayout from "@/components/layout/EmptyLayout";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import {
+  Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+
+import browserHistory from "@/helpers/history";
+
+import Home from "@/pages/Home";
+import Register from "@/pages/Register";
+import Login from "@/pages/Login";
+import { onLogout } from "@/stateManagement/userActionApi";
+import PublicRoute from "@/components/router/PublicRouter";
+import MainLayout from "@/components/layout/MainLayout";
+import EmptyLayout from "@/components/layout/EmptyLayout";
 
 import "@/styles/Main.css";
-import ChatContainer from "@/components/chatContainer/ChatContainer";
 
 class App extends PureComponent {
-  //
-  // componentDidMount() {
-  //   // const { onLogout } = this.props;
-  //   // if (isLoginTokenExpired()) onLogout();
-  // }
+
+  componentDidMount() {
+    // const { onLogout } = this.props;
+    // if (isLoginTokenExpired()) onLogout();
+  }
 
   render() {
-    return (<div>
-      {/*// <Router history={browserHistory}>*/}
-      {/*//   <Switch>*/}
-      {/*//     <MainLayout>*/}
-      {/*//       <Route path="/" exact component={Home} />*/}
-      {/*//     </MainLayout>*/}
-      {/*//     <EmptyLayout>*/}
-      {/*//       <PublicRoute path="/signup" component={Register} />*/}
-      {/*//     </EmptyLayout>*/}
-      {/*//     <EmptyLayout>*/}
-      {/*//       <PublicRoute path="/login" component={Login} />*/}
-      {/*//     </EmptyLayout>*/}
-      {/*//     <Redirect from="*" to="/" />*/}
-      {/*//   </Switch>*/}
-      {/*// </Router>);*/}
-      <ChatContainer />
-    </div>);
+    return (
+      <Router history={browserHistory}>
+        <Switch>
+          <MainLayout>
+            <Route path="/" exact component={Home} />
+          </MainLayout>
+          <EmptyLayout>
+            <PublicRoute path="/signup" component={Register} />
+          </EmptyLayout>
+          <EmptyLayout>
+            <PublicRoute path="/login" component={Login} />
+          </EmptyLayout>
+          <Redirect from="*" to="/" />
+        </Switch>
+      </Router>
+    );
   }
 }
 
-// const mapDispatchToProps = dispatch => bindActionCreators({ onLogout }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ onLogout }, dispatch);
 
-// export default connect(
-//   null,
-//   mapDispatchToProps
-// )(App);
-export default App
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
